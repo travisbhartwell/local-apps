9143375b6443224f7dbbfeac8ceaf488b28d31e1
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-13 13:37:27 -0600

    emacs-command needs to be on the path.

--- emacs-server/default.nix
+++ emacs-server/default.nix
@@ -12,18 +12,18 @@ stdenv.mkDerivation rec {
     cp -v ${./common-lib} "$out/share/emacs-server/common-lib"
     mkdir -p "$out/libexec/emacs-server"
     cp -v ${./connect-emacs} "$out/libexec/emacs-server/connect-emacs"
-    cp -v ${./emacs-command} "$out/libexec/emacs-server/emacs-command"
     cp -v ${./preload-emacs} "$out/libexec/emacs-server/preload-emacs"
     cp -v ${./start-emacs} "$out/libexec/emacs-server/start-emacs"
     mkdir -p "$out/bin"
     cp -v ${./emacs} "$out/bin/emacs"
+    cp -v ${./emacs-command} "$out/bin/emacs-command"
 
     substituteAllInPlace "$out/share/emacs-server/common-lib"
     substituteAllInPlace "$out/libexec/emacs-server/connect-emacs"
-    substituteAllInPlace "$out/libexec/emacs-server/emacs-command"
     substituteAllInPlace "$out/libexec/emacs-server/preload-emacs"
     substituteAllInPlace "$out/libexec/emacs-server/start-emacs"
     substituteAllInPlace "$out/bin/emacs"
+    substituteAllInPlace "$out/bin/emacs-command"
   '';
 
   meta = {
