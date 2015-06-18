f60a98a232487e50a55190d7bd5d81e1bade7113
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-18 06:44:07 -0600

    Add blog

--- local-apps.nix
+++ local-apps.nix
@@ -7,4 +7,5 @@ rec {
   travis-hartwell-mail = pkgs.callPackage ./travis-hartwell-mail { };
   emacs-server = pkgs.callPackage ./emacs-server { };
   gtkmenu = pkgs.haskellPackages.callPackage ./gtkmenu { inherit (pkgs) dmenu; };
+  iamtravis = pkgs.callPackage ../blog/iamtravis { pygments = pkgs.python27Packages.pygments; };
 }
