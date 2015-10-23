1e53a131a5f7aa3ad2936da7b1d88cabeb2b5c47
Travis B. Hartwell <nafai@travishartwell.net>
2015-10-22 20:59:46 -0600

    Use ghc7102 if none is specified.

--- gtkmenu/default.nix
+++ gtkmenu/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101", dmenu ? nixpkgs.pkgs.dmenu }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102", dmenu ? nixpkgs.pkgs.dmenu }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gtkmenu.nix { inherit (nixpkgs.pkgs) dmenu; }
