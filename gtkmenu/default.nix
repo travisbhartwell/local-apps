bdd53b962c3a25c88d771121e15a6cd649b0e66e
Travis B. Hartwell <nafai@travishartwell.net>
2016-10-31 09:13:18 -0600

    Updates

--- gtkmenu/default.nix
+++ gtkmenu/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7103", dmenu ? nixpkgs.pkgs.dmenu }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801", dmenu ? nixpkgs.pkgs.dmenu }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gtkmenu.nix { inherit (nixpkgs.pkgs) dmenu; }
--- gtkmenu/shell.nix
+++ gtkmenu/shell.nix
@@ -1,6 +1,6 @@
 {
   nixpkgs ? import <nixpkgs> {}
-, compiler ? "ghc7103"
+, compiler ? "ghc801"
 }:
 
 let
