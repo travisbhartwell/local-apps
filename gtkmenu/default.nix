d593626e685fdb6daf14f6f58f3b7966358dc527
Travis B. Hartwell <nafai@travishartwell.net>
2019-01-01 13:10:02 -0800

    Checkpoint package updates

--- gtkmenu/default.nix
+++ gtkmenu/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802", dmenu ? nixpkgs.pkgs.dmenu }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822", dmenu ? nixpkgs.pkgs.dmenu }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gtkmenu.nix { inherit (nixpkgs.pkgs) dmenu; }
